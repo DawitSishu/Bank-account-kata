edba39c360fe9bb94cb463cacc3366bad9a7ca6b
DawitSishu <dawitshishu@gmail.com>
2024-07-25 16:26:59 -0700

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,176 @@
-import React from 'react'
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import logo from "./assets/logo.png";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
+import ListAltIcon from "@mui/icons-material/ListAlt";
 
+const drawerWidth = 240;
 
 const App = () => {
-  return (
-    
-    <div>App</div>
-  )
-}
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [isClosing, setIsClosing] = React.useState(false);
+  const [selectedPage, setSelectedPage] = React.useState("Dashboard");
 
-export default App
+  const handleDrawerClose = () => {
+    setIsClosing(true);
+    setMobileOpen(false);
+  };
+
+  const handleDrawerTransitionEnd = () => {
+    setIsClosing(false);
+  };
+
+  const handleDrawerToggle = () => {
+    if (!isClosing) {
+      setMobileOpen(!mobileOpen);
+    }
+  };
+
+  const handleListItemClick = (text) => {
+    setSelectedPage(text);
+    if (mobileOpen) {
+      handleDrawerClose();
+    }
+  };
+
+  const drawer = (
+    <div
+      style={{
+        background: "linear-gradient(135deg,  #f5f5f5, #aed8eb,)",
+        height: " 100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "16px",
+        }}
+      >
+        <img
+          src={logo}
+          alt="Example Image"
+          style={{ width: "150px", height: "auto" }}
+        />
+      </div>
+      <Divider />
+      <List>
+        {[
+          { text: "Dashboard", icon: <DashboardIcon /> },
+          { text: "Deposit", icon: <AccountBalanceWalletIcon /> },
+          { text: "Withdraw", icon: <AttachMoneyIcon /> },
+          { text: "Transfer", icon: <SwapHorizIcon /> },
+          { text: "Account Statement", icon: <ListAltIcon /> },
+        ].map(({ text, icon }) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton onClick={() => handleListItemClick(text)}>
+              <ListItemIcon>{icon}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            {selectedPage}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          container={null}
+          variant="temporary"
+          open={mobileOpen}
+          onTransitionEnd={handleDrawerTransitionEnd}
+          onClose={handleDrawerClose}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Typography variant="h4" gutterBottom>
+          {selectedPage}
+        </Typography>
+        <Typography paragraph>
+          {`This is the content for the ${selectedPage} page.`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default App;
