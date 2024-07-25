848f07a2c7dc1326ba13afac0f049c919ac8cca1
DawitSishu <dawitshishu@gmail.com>
2024-07-25 15:58:46 -0700

    basic styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#05223b",
+    },
+    secondary: {
+      main: "#EEE2B1",
+    },
+    tertiary: {
+      main: "#FFFFFF",
+    },
+    background: {},
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          background: "linear-gradient(130deg,#ffffff 20%, #a6cdf7 140%)",
+        },
+      },
+    },
+  },
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
